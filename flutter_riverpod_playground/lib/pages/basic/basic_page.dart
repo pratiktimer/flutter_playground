c0883ed5b2bbb67b10ca5be80c14de59580476f7
praban <pbanawalkar2@gmail.com>
2025-01-25 10:43:42 +0530

    consumerwidget implemented

--- flutter_riverpod_playground/lib/pages/basic/basic_page.dart
+++ flutter_riverpod_playground/lib/pages/basic/basic_page.dart
@@ -2,28 +2,51 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_riverpod_playground/pages/basic/basic_provider.dart';
 
-class BasicPage extends StatelessWidget {
+// class BasicPage extends StatelessWidget {
+//   const BasicPage({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Provider'),
+//       ),
+//       body: Center(
+//         child: Consumer(
+//           builder: (context, ref, child) {
+//             final hello = ref.watch(helloProvider);
+//             final world = ref.watch(worldProvider);
+//             return Text('$hello $world',
+//                 style: TextStyle(
+//                   fontSize: 18,
+//                   fontWeight: FontWeight.bold,
+//                 ));
+//           },
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+class BasicPage extends ConsumerWidget {
   const BasicPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final hello = ref.watch(helloProvider);
+    final world = ref.watch(worldProvider);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Provider'),
       ),
       body: Center(
-        child: Consumer(
-          builder: (context, ref, child) {
-            final hello = ref.watch(helloProvider);
-            final world = ref.watch(worldProvider);
-            return Text('$hello $world',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                ));
-          },
+          child: Text(
+        '$hello $world',
+        style: TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
         ),
-      ),
+      )),
     );
   }
 }
